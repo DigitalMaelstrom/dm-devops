6e62079d7be6192a93e6e960546c9a8ea0d037d5
Kristopher Stice-Hall <kristopher.stice@digitalmaelstrom.net>
2018-10-30 14:07:26 -0500

    Update provision-salt-minion-aws.sh
    Redirected more if statement errors to /dev/null

--- provision-salt-minion-aws.sh
+++ provision-salt-minion-aws.sh
@@ -38,7 +38,7 @@ is_service_loaded() {
       echo "0"
     fi
   else
-    if status $1 | grep -qi "^$1"; then
+    if status $1 2> /dev/null | grep -qi "^$1"; then
       echo "1"
     elif service $1 status | grep -qiv " unrecognized service$"; then
       echo "1"
@@ -56,7 +56,7 @@ is_service_active() {
       echo "0"
     fi
   else
-    if status $1 | grep -qi "^$1 start/running,"; then
+    if status $1 2> /dev/null | grep -qi "^$1 start/running,"; then
       echo "1"
     elif service $1 status | grep -qi " is running$"; then
       echo "1"
@@ -71,7 +71,7 @@ disable_service() {
     systemctl stop $1
     systemctl disable $1
   else
-    if status $1 | grep -qi "^$1"; then
+    if status $1 2> /dev/null | grep -qi "^$1"; then
       stop $1
     else
       service $1 stop
@@ -91,7 +91,7 @@ start_service() {
   if which systemctl > /dev/null 2> /dev/null; then
     systemctl start $1
   else
-    if status $1 | grep -qi "^$1"; then
+    if status $1 2> /dev/null | grep -qi "^$1"; then
       start $1
     else
       service $1 start
@@ -100,25 +100,25 @@ start_service() {
 }
 
 is_package_installed() {
-  if which apt > /dev/null; then
-    if apt list $1 --installed | grep -qi "^$1\/"; then echo "1"; else echo "0"; fi
+  if which apt > /dev/null 2> /dev/null; then
+    if apt list $1 --installed 2> /dev/null | grep -qi "^$1\/"; then echo "1"; else echo "0"; fi
   else
-    if yum list installed $1 | grep -qi "^$1\."; then echo "1"; else echo "0"; fi
+    if yum list installed $1 2> /dev/null | grep -qi "^$1\."; then echo "1"; else echo "0"; fi
   fi
 }
 
 print "checking for required common commands ..."
 let python_command
-if which python > /dev/null; then
+if which python > /dev/null 2> /dev/null; then
   python_command=python
-elif which python3 > /dev/null; then
+elif which python3 > /dev/null 2> /dev/null; then
   python_command=python3
 else
   print_error "python not found"
   exit 1
 fi
 for command in curl grep; do
-  if ! which ${command} > /dev/null; then
+  if ! which ${command} > /dev/null 2> /dev/null; then
     print_error "${command} not found"
     exit 1
   fi
@@ -194,7 +194,7 @@ else
   print "systems manager agent is running"
 fi
 
-if which salt-call > /dev/null; then
+if which salt-call > /dev/null 2> /dev/null; then
   print "salt minion already installed"
 else
   print "installing salt minion ..."
@@ -215,7 +215,7 @@ else
     curl -L https://bootstrap.saltstack.com -o bootstrap_salt.sh
     sh bootstrap_salt.sh
   fi
-  if ! which salt-call > /dev/null; then
+  if ! which salt-call > /dev/null 2> /dev/null; then
     print_error "salt minion installation failed"
     exit 1
   fi
