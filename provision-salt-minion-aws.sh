6732eae49f7812f61dd3d65ea8d56ba1820fcf05
Kristopher Stice-Hall <kristopher.stice@digitalmaelstrom.net>
2018-10-30 14:22:41 -0500

    Update provision-salt-minion-aws.sh
    Removed cloud init items

--- provision-salt-minion-aws.sh
+++ provision-salt-minion-aws.sh
@@ -1,22 +1,3 @@
-Content-Type: multipart/mixed; boundary="//"
-MIME-Version: 1.0
-
---//
-Content-Type: text/cloud-config; charset="us-ascii"
-MIME-Version: 1.0
-Content-Transfer-Encoding: 7bit
-Content-Disposition: attachment; filename="cloud-config.txt"
-
-#cloud-config
-cloud_final_modules:
-  - [scripts-user, always]
-
---//
-Content-Type: text/x-shellscript; charset="us-ascii"
-MIME-Version: 1.0
-Content-Transfer-Encoding: 7bit
-Content-Disposition: attachment; filename="userdata.txt"
-
 #!/bin/bash
 
 print() {
@@ -227,6 +208,4 @@ disable_service salt-minion
 
 print "all succeeded"
 print ""
-
---//
 
